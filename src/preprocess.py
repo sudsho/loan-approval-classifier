6a5dff5afa85ebdb311d4747d0f41162aea8914b
Sudhanshu Srivastava <48932266+sudsho@users.noreply.github.com>
2019-09-10 11:15:29 +0530

    imputation + label encoding

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,5 +1,6 @@
 """Preprocessing for loan approval data."""
 import pandas as pd
+from sklearn.preprocessing import LabelEncoder
 
 
 NUMERIC_COLS = [
@@ -19,6 +20,8 @@ CATEGORICAL_COLS = [
     'Property_Area',
 ]
 
+TARGET = 'Loan_Status'
+
 
 def load_csv(path):
     return pd.read_csv(path)
@@ -29,3 +32,38 @@ def basic_clean(df):
     if 'Loan_ID' in df.columns:
         df = df.drop(columns=['Loan_ID'])
     return df
+
+
+def impute_missing(df):
+    df = df.copy()
+    for c in NUMERIC_COLS:
+        if c in df.columns:
+            df[c] = df[c].fillna(df[c].median())
+    for c in CATEGORICAL_COLS:
+        if c in df.columns:
+            df[c] = df[c].fillna(df[c].mode()[0])
+    return df
+
+
+def encode_categoricals(df, encoders=None):
+    df = df.copy()
+    fitted = {}
+    for c in CATEGORICAL_COLS:
+        if c not in df.columns:
+            continue
+        if encoders and c in encoders:
+            le = encoders[c]
+            # fall back to most frequent for unseen labels
+            most_common = le.classes_[0]
+            df[c] = df[c].apply(lambda x: x if x in le.classes_ else most_common)
+            df[c] = le.transform(df[c].astype(str))
+        else:
+            le = LabelEncoder()
+            df[c] = le.fit_transform(df[c].astype(str))
+            fitted[c] = le
+    return df, fitted
+
+
+def encode_target(y):
+    # Y -> 1, N -> 0
+    return (y == 'Y').astype(int)
